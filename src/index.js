982a182f34a83912cbe3a212690c20f7b38c66c8
MahmoudAbusaqer <mahmoudabusaqer1@gmail.com>
2022-05-25 23:00:26 +0300

    Front-end and the smart contract are done, and Moralis is connected to the code

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from 'web3uikit';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MoralisProvider appId="k8iMlzMC1kx7jK5QWbaEWXfaZSXrFpk4PbfrEmPA" serverUrl="https://g86sy3hd172t.usemoralis.com:2053/server">
+      <NotificationProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      </NotificationProvider>
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
